ffc8fff87932928caaa133c112353905641783cb
leonardo de santis <leonardo.desantis@yahoo.it>
2017-04-16 17:03:01 +0200

    objset corrections

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -227,14 +227,16 @@ object GoogleVsApple {
     val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
     val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-    lazy val googleTweets: TweetSet = ???
-    lazy val appleTweets: TweetSet = ???
+    def getTweetFor(brand: List[String]) = TweetReader.allTweets.filter(tweet => brand.exists(word => tweet.text.contains(word)))
+
+    lazy val googleTweets: TweetSet = getTweetFor(google)
+    lazy val appleTweets: TweetSet = getTweetFor(apple)
 
     /**
       * A list of all tweets mentioning a keyword from either apple or google,
       * sorted by the number of retweets.
       */
-    lazy val trending: TweetList = ???
+    lazy val trending: TweetList = googleTweets.union(appleTweets).descendingByRetweet
 }
 
 object Main extends App {
